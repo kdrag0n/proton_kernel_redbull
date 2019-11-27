c593fc974efc28f702a0321d9d39b954152b2d07
Mark Salyzyn <salyzyn@google.com>
2019-11-27 08:30:57 -0800

    GKI: msm: audio: suppress unknown object warning trace
    It is possible to not have a latency_pm_qos_req active during
    asynchronous attempted probing from USB devices, especially, suspect
    when modularized.  Get rid of the annoying and alarming kernel
    log spam.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Test: warning no longer pops up during boot up
    Change-Id: I67c5e1b9e0f703c872ba1e88673e32e39f484b18

--- asoc/msm-pcm-hostless.c
+++ asoc/msm-pcm-hostless.c
@@ -18,7 +18,8 @@ static int msm_pcm_hostless_prepare(struct snd_pcm_substream *substream)
 		pr_err("%s: invalid params\n", __func__);
 		return -EINVAL;
 	}
-	pm_qos_remove_request(&substream->latency_pm_qos_req);
+	if (pm_qos_request_active(&substream->latency_pm_qos_req))
+		pm_qos_remove_request(&substream->latency_pm_qos_req);
 	return 0;
 }
 
