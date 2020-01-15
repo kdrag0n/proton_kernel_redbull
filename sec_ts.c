87f5b85a0163ebd6a6ade5311d6ef04986f27c7c
Super Liu <supercjliu@google.com>
2020-01-15 15:03:43 +0800

    sec_touch: add io delay for some commands
    Bug: 145788304
    Change-Id: I7fb2fc399d9a259813aa896ce11d7c9f0a253a7d
    Signed-off-by: Super Liu <supercjliu@google.com>

--- sec_ts.c
+++ sec_ts.c
@@ -880,6 +880,14 @@ int sec_ts_spi_delay(u8 reg)
 	switch (reg) {
 	case SEC_TS_READ_TOUCH_RAWDATA:
 		return 400;
+	case SEC_TS_CMD_HEATMAP_READ:
+		return 500;
+	case SEC_TS_READ_ALL_EVENT:
+		return 500;
+	case SEC_TS_READ_CSRAM_RTDP_DATA:
+		return 500;
+	case SEC_TS_CAAT_READ_STORED_DATA:
+		return 500;
 	case SEC_TS_CMD_FLASH_READ_DATA:
 		return 1800;
 	case SEC_TS_READ_FIRMWARE_INTEGRITY:
