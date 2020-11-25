6596bc9d065a99a17aa74f343822c529b8b60a6a
Danny Lin <danny@kdrag0n.dev>
2020-11-24 17:49:33 -0800

    disp: rotator: Fix build with event log on and debugfs off
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- techpack/display/rotator/sde_rotator_debug.h
+++ techpack/display/rotator/sde_rotator_debug.h
@@ -34,7 +34,7 @@ enum sde_rot_dbg_evtlog_flag {
 	sde_rot_evtlog_tout_handler(false, __func__, ##__VA_ARGS__, \
 		SDE_ROT_EVTLOG_TOUT_DATA_LIMITER)
 
-#if defined(CONFIG_MSM_SDE_ROTATOR_EVTLOG_DEBUG) && \
+#if defined(CONFIG_MSM_SDE_ROTATOR_EVTLOG_DEBUG) || \
 	defined(CONFIG_DEBUG_FS)
 void sde_rot_evtlog(const char *name, int line, int flag, ...);
 void sde_rot_evtlog_tout_handler(bool queue, const char *name, ...);
