e8280d7fede8b74358db01d4194852cc73a918dd
Danny Lin <danny@kdrag0n.dev>
2020-12-04 18:26:09 -0800

    Rename flash to init and create empty directories

--- packrd-gz.sh
+++ packrd-gz.sh
@@ -3,6 +3,11 @@
 set -ve
 
 cd "$(dirname "$0")"
+
+cp -af init.sh rd/init
+mkdir -p rd/{tmp,sys,srv,run,root,proc,opt,mnt,home,dev}
+mkdir -p rd/var/{tmp,opt,mail,log,local,empty}
+
 oldwd="$PWD"
 cd rd
 find . | cpio -o -H newc | pigz -9c > "$oldwd/rd-new.cpio"
