33fe1a173a712c108043579f6c88ca1c99eff15d
Danny Lin <danny@kdrag0n.dev>
2020-12-15 17:25:11 -0800

    proc: cmdline: Don't patch flags in recovery mode
    fastbootd checks these flags to determine whether to allow flashing
    images. Flashing is disallowed if androidboot.flash.locked=1:
    
    Resizing 'system_a'                                FAILED (remote: 'Command not available on locked devices')
    fastboot: error: Command failed
    
    Skip patching flags when booted into recovery to fix flashing on
    unlocked bootloaders.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -38,6 +38,15 @@ static void patch_safetynet_flags(char *cmd)
 	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
 }
 
+static bool in_recovery;
+
+static int __init boot_mode_setup(char *value)
+{
+	in_recovery = !strcmp(value, "recovery");
+	return 1;
+}
+__setup("androidboot.mode=", boot_mode_setup);
+
 static int __init proc_cmdline_init(void)
 {
 	strcpy(new_command_line, saved_command_line);
@@ -46,7 +55,8 @@ static int __init proc_cmdline_init(void)
 	 * Patch various flags from command line seen by userspace in order to
 	 * pass SafetyNet checks.
 	 */
-	patch_safetynet_flags(new_command_line);
+	if (!in_recovery)
+		patch_safetynet_flags(new_command_line);
 
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
